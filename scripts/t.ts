d2cbcac079deb54eb0652d3518ed21cb09b0d195
Bobby Galli <bobbyg603@gmail.com>
2021-04-17 17:57:29 -0400

    Unify Processing of Single File and Directory cases Fixes #9

--- scripts/t.ts
+++ scripts/t.ts
@@ -1,25 +1,24 @@
 import { StackConverter } from "../src/stackconverter";
 import * as fs from "fs";
 
-
 const underlineString = (str: string) => str + "\n" + '-'.repeat(str.length);
 
 (async () => {
     try {
         if (process.argv.length != 4) {
-            console.log('usage: t.ts mapfile stackfile');
+            console.log('usage: t.ts directory stackfile');
             process.exit(1);
         }
 
-        const mapfile = process.argv[2];
+        const sourceMapDir = process.argv[2];
         const stackFile = process.argv[3];
 
-        const converter = new StackConverter(mapfile);
-        await converter.init();
+        const converter = await StackConverter.createFromDirectory(sourceMapDir);
         const stackString = fs.readFileSync(stackFile, 'utf-8');
-        const newStackString = converter.convert(stackString);
+        const result = await converter.convert(stackString);
         console.log(underlineString('newStackString'));
-        console.log(newStackString);
+        console.log("error: "  + result.error);
+        console.log("stack: \n" + result.stack)
     } catch (e) {
         console.log(`bad things happened e= ${e}`);
     }
